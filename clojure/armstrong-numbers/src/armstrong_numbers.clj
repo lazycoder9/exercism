8b7a25af5304549bb1ca101b66168fd8328255f9
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-08-08 20:32:21 +0500

    improve armstrong-numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,12 +1,10 @@
 (ns armstrong-numbers)
 
-(defn int-to-digits [num]
-  (->> num str (map #(Character/digit % 10))))
-
-(defn armstrong? [num] 
-  (let [digits (int-to-digits num)
-        power (count digits)]
-    (->> digits
-         (map #(Math/pow % power)) 
-         (reduce + ) 
+(defn armstrong? [num]
+  (let [power (-> num str seq count)]
+    (->> num
+         str
+         (map #(Character/digit % 10))
+         (map #(Math/pow % power))
+         (reduce +) 
          (== num))))
