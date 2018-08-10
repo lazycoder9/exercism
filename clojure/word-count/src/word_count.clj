a4302923579131cdf793b04286e2fef37701d2c5
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-08-10 15:32:57 +0500

    improve clojure/word-count

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -2,7 +2,4 @@
   (:require [clojure.string :as str]))
 
 (defn word-count [s]
-  (->> s
-       (re-seq #"\w+")
-       (map #(str/lower-case %))
-       (reduce #(assoc %1 %2 (inc (%1 %2 0))) {})))
+  (->> s str/lower-case (re-seq #"\w+") frequencies))
